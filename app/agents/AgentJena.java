9ad8e80e26fa68b62b148bc1428ca1e8df073260
ytexier <yohan.texier@gmail.com>
2014-01-18 12:39:26 +0100

    check

--- app/agents/AgentJena.java
+++ app/agents/AgentJena.java
@@ -51,7 +51,6 @@ public class AgentJena extends AgentManager{
 			.addProperty(VCARD.NICKNAME, user.getNickName())
 			.addProperty(VCARD.EMAIL, user.getEmail());
 		
-		printStatements();
 		//Vita.model.write(System.out, "RDF/XML-ABBREV");
 	}
 	
@@ -150,36 +149,7 @@ public class AgentJena extends AgentManager{
 	@Override
 	public void spy(Mammal mammal) {
 	}
-	
-   
-	
-	public static void printStatements(){
-		
-		Logger.debug("[[[[PRINT STATEMENTS]]]]");
-		
-		// list the statements in the Model
-		StmtIterator iter = model.listStatements();
 
-		// print out the predicate, subject and object of each statement
-		while (iter.hasNext()) 
-		{
-			Statement stmt      = iter.nextStatement();  // get next statement
-			Resource  subject   = stmt.getSubject();     // get the subject
-			Property  predicate = stmt.getPredicate();   // get the predicate
-			RDFNode   object    = stmt.getObject();      // get the object
-
-			Logger.debug(subject.toString());
-			Logger.debug(" " + predicate.toString() + " ");
-			if (object instanceof Resource) {
-				Logger.debug(object.toString());
-			} else {
-				// object is a literal
-				Logger.debug(" \"" + object.toString() + "\"");
-			}
-
-			Logger.debug(" .");
-		} 
-	}
 
 
 }
