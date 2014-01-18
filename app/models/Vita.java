41cf815bbf711f8fd757352d0ab19a4e1658e060
ytexier <yohan.texier@gmail.com>
2014-01-18 03:57:27 +0100

    jj

--- app/models/Vita.java
+++ app/models/Vita.java
@@ -1,8 +1,7 @@
 package models;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.awt.List;
+import java.util.ArrayList;
 
 import com.hp.hpl.jena.ontology.ObjectProperty;
 import com.hp.hpl.jena.ontology.OntClass;
@@ -25,8 +24,10 @@ public class Vita {
 	private static OntModel omVita;
 	
 	String vitaOntology_url = "test/vita.owl";
-	
 
+	/**
+	 * CLASSES DEF
+	 */	
 	OntClass ocActivity;
 	OntClass ocOrganism;
 	OntClass ocEquipment;
@@ -37,8 +38,18 @@ public class Vita {
 	OntClass ocPicture;
 	OntClass ocSector;
 	
-	
-	
+	/**
+	 * PROPERTIES DEF
+	 */	
+	ObjectProperty targetedOrganism;
+	ObjectProperty usedEquipment;
+	ObjectProperty isRelatedToEvent;
+	ObjectProperty hasLocation;
+	ObjectProperty hasDescription;
+	ObjectProperty hasPicture;
+	ObjectProperty hasSector;
+	ObjectProperty hasShowcase;
+
 	public Vita() {
 
 		omVita = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
@@ -48,15 +59,15 @@ public class Vita {
 	}
 	
 	public void addClasses() {
-		ocActivity 		= omVita.createClass( vita_ns + "Activity" );
-		ocOrganism 		= omVita.createClass( vita_ns + "Organism" );
+		ocActivity 		= omVita.createClass( vita_ns + "Activity" 	);
+		ocOrganism 		= omVita.createClass( vita_ns + "Organism" 	);
 		ocEquipment 	= omVita.createClass( vita_ns + "Equipment" );
-		ocEvent 		= omVita.createClass( vita_ns + "Event" );
-		ocLocation 		= omVita.createClass( vita_ns + "Location" );
-		ocShowcase 		= omVita.createClass( vita_ns + "Showcase" );
+		ocEvent 		= omVita.createClass( vita_ns + "Event" 	);
+		ocLocation 		= omVita.createClass( vita_ns + "Location" 	);
+		ocShowcase 		= omVita.createClass( vita_ns + "Showcase" 	);
 		ocDescription 	= omVita.createClass( vita_ns + "Description" );
-		ocPicture 		= omVita.createClass( vita_ns + "Picture" );
-		ocSector 		= omVita.createClass( vita_ns + "Sector" );
+		ocPicture 		= omVita.createClass( vita_ns + "Picture" 	);
+		ocSector 		= omVita.createClass( vita_ns + "Sector" 	);
 	}
 
 	public void addProperties() {
@@ -65,21 +76,15 @@ public class Vita {
 		 * PROPERTIES : Domain ACTIVITY
 		 */	
 		
-		//targetedOrganism
-		ObjectProperty targetedOrganism 
-			= omVita.createObjectProperty( vita_ns + "targetedOrganism" );
+		targetedOrganism = omVita.createObjectProperty(vita_ns+"targetedOrganism");
 		targetedOrganism.addRange(ocOrganism);
 		targetedOrganism.addDomain(ocActivity);
-		
-		//usedEquipment
-		ObjectProperty usedEquipment 
-			= omVita.createObjectProperty( vita_ns + "usedEquipment" );
+
+		usedEquipment = omVita.createObjectProperty(vita_ns+"usedEquipment");
 		usedEquipment.addRange(ocEquipment);
 		usedEquipment.addDomain(ocActivity);
 		
-		//isRelatedToEvent
-		ObjectProperty isRelatedToEvent 
-			= omVita.createObjectProperty( vita_ns + "isRelatedToEvent" );
+		isRelatedToEvent = omVita.createObjectProperty( vita_ns + "isRelatedToEvent" );
 		isRelatedToEvent.addRange(ocEvent);
 		isRelatedToEvent.addDomain(ocActivity);
 
@@ -87,28 +92,20 @@ public class Vita {
 		 * PROPERTIES : DOMAIN Showcase
 		 */	
 		
-		//hasLocation
-		ObjectProperty hasLocation 
-			= omVita.createObjectProperty( vita_ns + "hasLocation" );
+		hasLocation = omVita.createObjectProperty( vita_ns + "hasLocation" );
 		hasLocation.addRange(ocLocation);
 		hasLocation.addDomain(ocShowcase);
 			
-		//hasDescription
-		ObjectProperty hasDescription 
-			= omVita.createObjectProperty( vita_ns + "hasDescription" );
+		hasDescription = omVita.createObjectProperty( vita_ns + "hasDescription" );
 		hasDescription.addRange(ocDescription);
 		hasLocation.addDomain(ocShowcase);
 		hasDescription.addIsDefinedBy(RDFS.Literal);
 			
-		//hasPicture
-		ObjectProperty hasPicture 
-			= omVita.createObjectProperty( vita_ns + "hasPicture" );
+		hasPicture = omVita.createObjectProperty( vita_ns + "hasPicture" );
 		hasPicture.addRange(ocPicture);
 		hasPicture.addDomain(ocShowcase);
 			
-		//hasPicture
-		ObjectProperty hasSector 
-			= omVita.createObjectProperty( vita_ns + "hasPicture" );
+		hasSector = omVita.createObjectProperty( vita_ns + "hasPicture" );
 		hasSector.addRange(ocSector);
 		hasSector.addDomain(ocShowcase);
 
@@ -116,15 +113,25 @@ public class Vita {
 		 * PROPERTIES : RANGE Showcase
 		 */	
 			
-		//hasShowcase
-		ObjectProperty hasShowcase 
-			= omVita.createObjectProperty( vita_ns + "hasShowcase" );
+		hasShowcase = omVita.createObjectProperty( vita_ns + "hasShowcase" );
 		hasShowcase.addRange(ocShowcase);
+		hasSector.addDomain(ocActivity);
+		hasSector.addDomain(ocEquipment);
+		hasSector.addDomain(ocOrganism);
+		hasSector.addDomain(ocEvent);
 		
 	 }
-	 
+	
 	 
 	public String toString(){
+		
+		ArrayList<String> classes = new ArrayList<String>();
+		classes.add("\n/**\n* CLASSES\n*/");
+		ArrayList<String> properties = new ArrayList<String>();
+		properties.add("\n/**\n* PROPERTIES\n*/");
+		ArrayList<String> others = new ArrayList<String>();
+		others.add("\n/**\n* OTHERS\n*/");
+	
 		String res = "";
 		StmtIterator iter = omVita.listStatements();
 		while (iter.hasNext()) 
@@ -134,23 +141,40 @@ public class Vita {
 			Property  predicate = stmt.getPredicate();   // get the predicate
 			RDFNode   object    = stmt.getObject();      // get the object
 
+			String subject_split = subject.toString().substring(subject.toString().lastIndexOf("#")+1);
+			String object_split = object.toString().substring(object.toString().lastIndexOf("#")+1);
+			
 			res+="\n";
-			res+="//"+subject.toString().substring(subject.toString().lastIndexOf("#"));
+			res+="\t##"+subject_split;
 			res+="\n";
-			res+="[subject]:";
+			res+="\t[subject]:";
 			res+=subject.toString();
 			res+="\n";
-			res+="[predicate]:";
+			res+="\t[predicate]:";
 			res+=predicate.toString();
 			res+="\n";
-			res+="[object]:";
+			res+="\t[object]:";
 			if (object instanceof Resource)
 				res+=object.toString();
 			else
 				res+= "\"" + object.toString() + "\"";
 			
 			res+="\n";
+			
+			if(object_split.equals("Class")){
+				classes.add(res);
+			}else if(object_split.equals("ObjectProperty")){
+				properties.add(res);
+			}else{
+				others.add(res);
+			}
+			
+			
+			res = "";
 		} 
-		return res;
+		
+		properties.addAll(others);
+		classes.addAll(properties);
+		return classes.toString();
 	}
 }
