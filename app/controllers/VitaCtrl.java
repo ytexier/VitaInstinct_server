244e76590812b2123b83da9ac073ca2e00ab83c0
LuckyBanana <dimitri.varet@gmail.com>
2014-01-21 17:38:10 +0100

    sparql

--- app/controllers/VitaCtrl.java
+++ app/controllers/VitaCtrl.java
@@ -8,9 +8,13 @@ import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 
 import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.ReadWrite;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
 import com.hp.hpl.jena.tdb.TDBFactory;
 
 import models.VitaOWL;
@@ -19,17 +23,57 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class VitaCtrl extends Controller{
-	
+
 	public static Result make() throws FileNotFoundException{
-		
-        String directory = "dataset" ;
-        Dataset dataset = TDBFactory.createDataset(directory) ;
-        dataset.begin(ReadWrite.READ) ;
-        Model model = dataset.getDefaultModel();
-        model.write(System.out, "RDF/XML-ABBREV");
-        dataset.end() ;
-		
-		
+
+		String directory = "dataset" ;
+		Dataset dataset = TDBFactory.createDataset(directory) ;
+		dataset.begin(ReadWrite.READ) ;
+		Model model = dataset.getDefaultModel();
+		model.write(System.out, "RDF/XML-ABBREV");
+		dataset.end() ;
+
+
 		return ok(new VitaOWL().toString());
 	}
+
+	public static Result test(String organism) {
+		Model model = ModelFactory.createDefaultModel();
+		String result = "Sanglier\n";
+		try {
+			String service = "http://fr.dbpedia.org/sparql";
+			String query = ""
+					+ "PREFIX db-owl: <http://dbpedia.org/ontology/>"
+					+ "PREFIX dbpedia: <http://fr.dbpedia.org/resource/>"
+					+ "select ?resume ?image where { "
+					+ "dbpedia:"+organism+" db-owl:abstract ?resume."
+					+ "dbpedia:"+organism+" db-owl:thumbnail ?image."
+					+ "filter(langMatches(lang(?resume), \"fr\"))"
+					+ "}"
+					;
+			QueryExecution qe = QueryExecutionFactory.sparqlService(service, query);
+			try {
+				ResultSet results = qe.execSelect();
+				for (; results.hasNext();) {
+					result += results.next().toString()+"\n";
+					// Result processing is done here.
+				}
+			} catch (QueryExceptionHTTP e) {
+				System.out.println(service + " is DOWN");
+			} finally {
+				qe.close();
+			} 
+
+
+		}
+		catch(Exception e) {
+			String sss = e.getMessage();
+			for(StackTraceElement el : e.getStackTrace()) {
+				sss += "\n"+el.toString();
+			}
+			return ok(sss);
+		}
+
+		return ok(result);
+	} 
 }
