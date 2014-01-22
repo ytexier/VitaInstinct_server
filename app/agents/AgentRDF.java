7742fe6a334130cd2343503c71e4d3c130d6541b
ytexier <yohan.texier@gmail.com>
2014-01-22 23:05:49 +0100

    ..

---
+++ app/agents/AgentRDF.java
@@ -0,0 +1,85 @@
+package agents;
+
+import models.Organism;
+import models.User;
+import models.fishing.FishingActivity;
+import models.fishing.FishingEquipment;
+import models.fishing.FishingEvent;
+import models.hunting.HuntingActivity;
+import models.hunting.HuntingEquipment;
+import models.hunting.HuntingEvent;
+import models.picking.PickingActivity;
+import models.picking.PickingEquipment;
+import models.picking.PickingEvent;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class AgentRDF extends AgentManager{
+
+	@Override
+	public void spy(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Model spy(HuntingActivity activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(PickingActivity activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(FishingActivity activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(HuntingEquipment equipment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(PickingEquipment equipment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(FishingEquipment equipment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(HuntingEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(PickingEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(FishingEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Model spy(Organism organism) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
